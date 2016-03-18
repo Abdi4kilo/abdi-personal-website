b4836d0bd980b541fcd9d107f1acc74c87cceed4
Abdi Biru <abdi.g.biru@gmail.com>
2016-03-18 02:08:08 -0400

    parallax scroll working but conflict with pervious navbar scroll

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
 
+var divid="";
+var off=0;
 $(".navi").click(function(e){
  			e.preventDefault();
- 			var divid="#"+$(this).attr('id')+"Div";
- 			
+ 			 divid="#"+$(this).attr('id')+"Div";
+ 			off=$(divid).offset().top+200
  			
  			$("html body").animate({
- 				scrollTop: $(divid).offset().top-55
+ 				scrollTop: off
  							},1000);
 
  		})
+
+		
+$('window').scroll(function(){
+	var ypos,image,image2;
+			//ypos=window.pageYOffset;
+			image=document.getElementById('par');
+			//image2=document.getElementById('experDiv');
+			image.style.top=off*0.6+"px";
+});
+
