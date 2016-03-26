2c87e2bf637341b366dd0479a7c361a7e333adcc
Abdi Biru <abdi.g.biru@gmail.com>
2016-03-25 21:32:07 -0400

    Changed backgorund of page used w3c html validator to correct html and add footer links and used awesome fonts instead

--- script/script.js
+++ script/script.js
@@ -1,17 +1,16 @@
-
 $(".navi").click(function(e){
- 			e.preventDefault();
- 			var divid="#"+$(this).attr('id')+"Div";
- 			
- 			
- 			$("html body").animate({
- 				scrollTop: $(divid).offset().top-55
- 							},1000);
+	
+	e.preventDefault();
+	
+	var divid="#"+$(this).attr('id')+"Div";
+	
+	$("html,body").animate({
+	scrollTop: $(divid).offset().top-55
+			},1000);
 
- 		})
-
+})
 function parallax(){
-	var ell=document.getElementById('uni');
+	var ell=document.getElementById('bgimage');
 	ell.style.top=-(window.pageYOffset*0.6)+'px';
 }
 window.addEventListener('scroll',parallax,false);
